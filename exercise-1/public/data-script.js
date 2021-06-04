23ad6778dabda8d3ed24c830b557a99771ff0028
Vladimir <storozhenko.vladimir.vasilhevich@gmail.com>
2021-06-04 13:17:17 +0300

    fix: import statement

--- exercise-1/public/data-script.js
+++ exercise-1/public/data-script.js
@@ -1,7 +1,33 @@
 const apiUrl = 'https://5dc588200bbd050014fb8ae1.mockapi.io/assessment';
 const templateUrl = 'users-list.hbr';
 
-import { normalizeUsers, toggleFields, fetchTemplate, renderHandlebarsTemplate} from '../libs'; 
+const renderHandlebarsTemplate = (withTemplate, inElement, withData) => {
+  const template = Handlebars.compile(withTemplate);
+  const element = document.querySelector(inElement);
+
+  element.innerHTML = template(withData);
+};
+
+const fetchTemplate = async (url) => {
+  const hbr = await fetch(url);
+  return await hbr.text();
+};
+
+const toggleFields = (fields) => (event) => {
+  event.target.classList.toggle('btn-toggle_clicked');
+  fields.forEach((element) => element.classList.toggle('hidden'));
+};
+
+const formatDate = (asString) => {
+  const asDateObj = new Date(asString);
+  return asDateObj.toLocaleString();
+};
+
+const normalizeUsers = (users) =>
+  users.map(({ createdAt, ...user }) => ({
+    ...user,
+    createdAt: formatDate(createdAt),
+  }));
 
 document.addEventListener('DOMContentLoaded', async function () {
   const response = await fetch(apiUrl);
