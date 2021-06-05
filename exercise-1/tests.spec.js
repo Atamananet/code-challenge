a1ebec21d711435494161916e0ad84dbac923da9
Vladimir <storozhenko.vladimir.vasilhevich@gmail.com>
2021-06-05 17:49:46 +0300

    feat: UI tests

--- exercise-1/tests.spec.js
+++ exercise-1/tests.spec.js
@@ -1,52 +1,70 @@
 const { normalizeUsers, toggleField } = require('./public/data-script');
+const puppeteer = require('puppeteer');
+const faker = require('faker');
+
+const APP = 'http://localhost:3000/index.html';
+let page;
+let browser;
+const width = 1024;
+const height = 768;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=${width},${height}`],
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+  await page.goto('http://localhost:3000');
+});
+
+afterAll(() => {
+  browser.close();
+});
+
+describe('UI tests', () => {
+  test('displays a user after a click', async () => {
+    await page.waitForSelector('.users-container');
+
+    await page.evaluate(() => {
+      document.querySelectorAll('.btn-toggle').forEach(button => button.click());
+    });
+
+    const isClicked = await page.evaluate(() => (
+      document.querySelector('.btn-toggle').classList.contains('btn-toggle_clicked')
+    ));
+    expect(isClicked).toBe(true);
+
+    const isHidden = await page.evaluate(() => (
+      document.querySelector('.addition').classList.contains('hidden')
+    ));
+    expect(isHidden).toBe(false);
+  }, 16000);
+
+  test('displays a user after a click', async () => {
+    await page.waitForSelector('.users-container');
+    await page.hover('button.btn_primary');
+    await page.screenshot({ path: 'hover.png' });
+  }, 16000);
+});
+
+const getFakeUser = () => ({
+  name: faker.name.firstName(),
+  id: faker.internet.email(),
+  createdAt: faker.date.past(1),
+  avatar: faker.internet.avatar()
+});
 
 const users = [
-  {
-    id: '14',
-    createdAt: '2021-03-31T15:59:27.302Z',
-    name: 'Tina White',
-    avatar: '',
-  },
-  {
-    id: '15',
-    createdAt: '2021-03-31T10:02:15.415Z',
-    name: 'Allen Durgan',
-    avatar: '',
-  },
-  {
-    id: '16',
-    createdAt: '2021-03-31T13:24:14.020Z',
-    name: 'Ryann Wiegand',
-    avatar: '',
-  },
+  getFakeUser(),
+  getFakeUser(),
+  getFakeUser(),
 ];
 
 test('normalizes users data', () => {
   expect(normalizeUsers).toThrow();
   expect(normalizeUsers.bind(null, users)).not.toThrow();
   expect(normalizeUsers([])).toEqual([]);
-});
-
-test('displays a user after a click', () => {
-  document.body.innerHTML = `
-    <li class='user'>
-        <div
-          class='user__avatar'
-          style='background-image: url(https://www.serietotaal.nl/images/nieuws/t/6fbi52rzy3ly-full.jpg)'
-        ></div>
-        <h3 class='user__name'>${users[0].name}</h3>
-        <p class='user__id addition hidden'> ID: <b>${users[0].id}</b></p>
-        <i class='user__createdAt addition hidden'>${users[0].createdAt}</i>
-        <button class='btn-toggle btn_primary'></button>
-    </li>`;
-
-  document.addEventListener('click', toggleField)
-  
-  document.querySelector('.btn-toggle').click();
-  expect(document.querySelector('.user__id').classList.contains('hidden')).toBe(false);
-  expect(document.querySelector('.user__createdAt').classList.contains('hidden')).toBe(false);
-
-  document.querySelector('.btn-toggle').click();
-  expect(document.querySelector('.user__id').classList.contains('hidden')).toBe(true);
-  expect(document.querySelector('.user__createdAt').classList.contains('hidden')).toBe(true);
+  expect(normalizeUsers.bind(null, users)).not.toThrow();
 });
